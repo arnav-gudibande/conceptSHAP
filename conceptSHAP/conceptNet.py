edecef93e4db600a40b4f5290fd8d5fa1e546a12
Tony <tonyzhao0824@berkeley.edu>
2020-03-24 00:10:28 -0700

    Finish/improve loss function and training loop

--- conceptSHAP/conceptNet.py
+++ conceptSHAP/conceptNet.py
@@ -9,15 +9,17 @@ class ConceptNet(nn.Module):
         super(ConceptNet, self).__init__()
 
         # note: clusters.shape = (num_clusters, num_sentences_per_cluster, embedding_dim)
+        # Tony(1): one caveat here is that different clusters might have different num_sentences_per_cluster,
+        # it holds in their case but not sure about our k-means. We might need to manually enforce this by truncation
         embedding_dim = clusters.shape[2]
 
         self.clusters = nn.Parameter(torch.tensor(clusters))
         self.clusters.requires_grad = False
 
         # random init using uniform dist
-        self.concept = nn.Parameter(self.init_concept(n_concepts, embedding_dim)) # the trainable concept
+        self.concept = nn.Parameter(self.init_concept(n_concepts, embedding_dim), requires_grad=True) # the trainable concept
 
-        self.h_x = h_x # final layers of the transformer
+        self.h_x = h_x # final layers of the transformer # Tony(3): make sure weights are frozen
         self.n_concepts = n_concepts
 
     def init_concept(self, n_concepts, embedding_dim):
@@ -26,15 +28,20 @@ class ConceptNet(nn.Module):
         concept = (r_2 - r_1) * torch.rand(n_concepts, embedding_dim) + r_1
         return concept
 
-    def concept_loss(self, y_true, y_pred, saliency_score, score_abs):
-        loss = nn.BCEWithLogitsLoss()
-        output = loss(y_pred, y_true)
-
-        # TODO: add concept-sparsity regularization
-
-        return output
+    # moved to loss()
+    # def concept_loss_fn(self, y_true, y_pred, saliency_score, score_abs):
+    #     loss = nn.BCEWithLogitsLoss()
+    #     output = loss(y_pred, y_true)
+    #
+    #     # TODO: add concept-sparsity regularization
+    #
+    #     return output
 
     def forward(self, train_embedding):
+        """
+        :param train_embedding: shape (bs, embedding_dim)
+        :return:
+        """
 
         concept_normalized = F.normalize(self.concept, p=2, dim=0)
 
@@ -47,13 +54,33 @@ class ConceptNet(nn.Module):
         # calculating the saliency score between the concept and the cluster
         score_numerator = torch.dot(torch.mean(self.clusters, dim=1), concept_normalized)
         score_numerator_normalized = torch.sub(score_numerator, torch.mean(score_numerator, dim=1, keepdim=True))
-        score_abs = torch.abs(F.normalize(score_numerator_normalized, p=2, dim=0))
+        score_abs = torch.abs(F.normalize(score_numerator_normalized, p=2, dim=0)) # cov0_abs
         score_flat = torch.reshape(score_abs, (-1, self.n_concepts))
-        saliency_score = torch.dot(torch.t(score_flat), score_flat)
+        saliency_score = torch.dot(torch.t(score_flat), score_flat) # cov
 
         # passing projected activations through rest of model
-        output = self.h_x(proj)
+        y_pred = self.h_x(proj)
 
-        concept_loss = self.concept_loss(train_embedding, output, saliency_score, score_abs)
+        return y_pred, saliency_score, score_abs # all the stuff that will be used to calculate the loss
 
-        return concept_loss
+    def loss(self, train_embedding, train_y_true, regularize=False, l=5.):
+        """
+        This function will be called externally to feed data and get the loss
+        :param train_embedding:
+        :param train_y_true:
+        :param regularize:
+        :param l: lambda weights
+        :return:
+        """
+        # TODO Tony: check dimension of each tensor
+        y_pred, saliency_score, score_abs = self.forward(train_embedding)
+
+        loss = nn.BCEWithLogitsLoss()
+        loss_val = torch.mean(loss(y_pred, train_y_true))
+
+        if regularize:
+            reg_loss_1 = torch.mean(saliency_score - torch.eye(self.n_concepts))
+            reg_loss_2 = torch.mean(score_abs)
+            return loss_val + l * (reg_loss_1 + reg_loss_2)
+        else:
+            return loss_val
