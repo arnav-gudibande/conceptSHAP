2109d0fe9bf667338aa0992266667c0eeeafd318
Annamira O'Toole <annamira@uname.net>
2020-04-05 19:23:26 -0400

    updated with small_sentences data code, refactored code, NOT TESTED

--- model/bert_inference.py
+++ model/bert_inference.py
@@ -10,19 +10,59 @@ import os
 
 device = torch.device('cuda')
 
+
+# LOAD DATA
+# IN: filepath to directory where the movie review dataset is stored
+# OUT: movie review sentences as a pandas dataframe with 0 polarity for every datapoint
+
+def load_data(PATH)
+  small_df = pd.read_pickle(os.path.join(PATH,"Data/sentences_small.pkl"))
+  small_df["polarity"] = small_df.shape[0] * [0] 
+  return small_df
+
+
+# LOAD MODEL
+# IN: filepath to directory where the model weights are stored
+# OUT: loaded model and sentence tokenizer object
+
+def load_model(PATH)
+
+  config = BertConfig.from_pretrained(PATH + "/imdb_weights/config.json", output_hidden_states=True)
+  bert_model = BertForSequenceClassification.from_pretrained(PATH + "/imdb_weights/pytorch_model.bin",config=new_config)
+
+  # possibly redundant
+  bert_model.cuda()
+  bert_model.to(device)
+
+  BERTMODEL = "bert-base-uncased"
+  tokenizer = BertTokenizer.from_pretrained(BERTMODEL, do_lower_case=True)
+  print("model loaded...")
+  
+  return bert_model, tokenizer
+
+
+# PROCESS THE DATA
+# IN: dataframe of sentences, bert tokenizer
+# OUT: dataloader
+
 def process_dataframe(_dframe, _tokenizer):
+
   sentences = _dframe.sentence.values
-  sentences = ["[CLS] " + s for s in sentences] #causing error bc of data format
-  labels = _dframe.polarity.values #WONT WORK, says no such thing as polarity
+  sentences = [["[CLS]"] + s for s in sentences]
+  
+  labels = _dframe.polarity.values 
 
-  tokenized = [_tokenizer.tokenize(s) for s in sentences]
-  tokenized = [t[:(MAX_LEN_TRAIN-1)]+['SEP'] for t in tokenized]
+  # tokenized = [_tokenizer.tokenize(s) for s in sentences]
+  # tokenized = [t[:(MAX_LEN_TRAIN-1)]+['SEP'] for t in tokenized]
 
+  tokenized = [t[:(MAX_LEN_TRAIN-1)]+['SEP'] for t in sentences]
 
   ids = [_tokenizer.convert_tokens_to_ids(t) for t in tokenized]
   ids = np.array([np.pad(i, (0, MAX_LEN_TRAIN-len(i)),
                              mode='constant') for i in ids])
+  
   amasks = []
+  
   for seq in ids:
     seq_mask = [float(i>0) for i in seq]
     amasks.append(seq_mask)
@@ -35,7 +75,13 @@ def process_dataframe(_dframe, _tokenizer):
   sampler = SequentialSampler(data)
 
   dataloader = DataLoader(data, sampler=sampler, batch_size=1)
+  
   return dataloader
+
+
+# RUN THE MODEL
+# IN: dataloader, pre-loaded bert model
+# OUT: side effects
 
 def run_model(_model, loader):
 
@@ -43,25 +89,21 @@ def run_model(_model, loader):
     batch = tuple(t.to(device) for t in batch)
     b_input_ids, b_input_mask, b_labels = batch
 
-  with torch.no_grad():
-      outputs = _model(b_input_ids, token_type_ids=None,
-                      attention_mask=b_input_mask)
-      logits = outputs[0]
-  return outputs
+    # outputs doesn't need to be saved
+    with torch.no_grad():
+        outputs = _model(b_input_ids, token_type_ids=None,
+                        attention_mask=b_input_mask)
 
-# BERT MODEL INITIALIZATION
-# source code can be found here https://github.com/huggingface/transformers/blob/bb7c46852051f7d031dd4be0240c9c9db82f6ed9/src/transformers/modeling_bert.py#L1107
-PATH = "/home/arnav/Documents/intuit-project/model/"
-model = BertForSequenceClassification.from_pretrained(PATH + "imdb_weights") # load directly from checkpoint of imdb
-model.to(device) # move to gpu
 
-# PROCESS SIMPLE SENTENCE
-BERTMODEL = "bert-base-uncased"
-tokenizer = BertTokenizer.from_pretrained(BERTMODEL, do_lower_case=True)
-print("model loaded...")
+
+"""Notice here: the function above added a forward hook to "layer_idx" layer of our model. You might want to google "register_forward_hook" to fully understand it but in short, everytime something is fed into the model and through the layer we specified, the function "extract_activation_hook" will get called. And "extract_activation_hook" will save the layer output to EXTRACTED_ACTIVATIONS when RECORD is true."""
+MAX_LEN_TRAIN, MAX_LEN_TEST = 128, 512
+
+
 
 EXTRACTED_ACTIVATIONS = []
 RECORD = False
+
 def extract_activation_hook(module, input, output):
   if RECORD:
     EXTRACTED_ACTIVATIONS.append(output)
@@ -70,24 +112,40 @@ def add_activation_hook(model, layer_idx):
   all_modules_list = list(model.modules())
   module = all_modules_list[layer_idx]
   module.register_forward_hook(extract_activation_hook)
+ 
 
-add_activation_hook(model, layer_idx=-2)
 
-"""Notice here: the function above added a forward hook to "layer_idx" layer of our model. You might want to google "register_forward_hook" to fully understand it but in short, everytime something is fed into the model and through the layer we specified, the function "extract_activation_hook" will get called. And "extract_activation_hook" will save the layer output to EXTRACTED_ACTIVATIONS when RECORD is true."""
-MAX_LEN_TRAIN, MAX_LEN_TEST = 128, 512
+######################################################
+# ALL STEPS PUT TOGETHER
+######################################################
 
-test_sentence = {}
-test_sentence["sentence"] = ["this is a great movie, thumbs up!"]
-train_df = pd.DataFrame.from_dict(test_sentence)
-train_df["polarity"] = [0]
-loader = process_dataframe(train_df, tokenizer)
+# IN: filepath to data directory, filepath to model weights
+# OUT: embeddings
 
-RECORD=True
-result = run_model(model, loader) # run the whole model
-RECORD=False
+def get_sentence_activation(DATAPATH, MODELPATH):
 
-def get_sentence_activation():
+  sentence_df = load_data(DATAPATH)
+
+  model, tokenizer = lead_model(MODELPATH)
+
+  loader = process_dataframe(sentence_df, tokenizer)
+
+  add_activation_hook(model, layer_idx=-2)
+
+  RECORD=True
+  result = run_model(model, loader) # run the whole model
+  RECORD=False
+
   return result, EXTRACTED_ACTIVATIONS[-1]
+
+
+# IN: filepath to data directory, embeddings/activations
+# OUT: side effect = writing the activations to a .pkl file
+
+def save_activations(activations, DATAPATH):
+  outfile = open(os.path.join(DATAPATH,'small_activations.pkl'), 'wb') 
+  pickle.dump(activations, outfile)
+  outfile.close()
 
 
 '''
