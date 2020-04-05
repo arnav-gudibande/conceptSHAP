13b4a339562939cad6a2ef337761f0ce4c75d884
RyanJia <ryanjiaxc@gmail.com>
2020-04-05 12:33:56 -0700

    pipeline

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=1
+python3 train.py \
+	--activation_dir="../data/small_activations.npy" \
+    --cluster_dir="../data/small_clusters.npy" \
+    --train_dir="../data/sentences_small.pkl" \
+    --bert_weights="../model/imdb_weights" \
+    --n_concepts=5 \
+    --save_dir="./experiments" \
+    --log_dir="./logs" \
+    --lr=1e-3 \
+    --batch_size=32 \
+    --num_epochs=200 \
+    --loss_reg_epoch=5 \
+    --save_interval=50
