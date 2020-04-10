f52512f6acf794e9c5775592cdde09c51b9b0411
arnav-gudibande <arnav.gudibande@gmail.com>
2020-04-10 10:25:07 -0700

    added batch size args to main script

--- main.sh
+++ main.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
 # Please check the settings below before running the script
 CUDA_VISIBLE_DEVICES=1
 batchSize=64
 
 # Preprocess arguments
-isDownloaded=0  # whether we already have the data; if set to 0, the script will download the data automatically
+isDownloaded=1  # whether we already have the data; if set to 0, the script will download the data automatically
 downloadedPath="data/imdb"  # path to the directory of original data
 size=1000  # number of training sentences to run fragment extractions on
-runOption=1  # 1 for both, 2 for getting sentence fragments, 3 for storing the train/test pickles to train BERT
-trainDir="data/sentence_fragments.pkl"  # sliding window data for extracting inference
+runOption=2  # 1 for both, 2 for getting sentence fragments, 3 for storing the train/test pickles to train BERT
+trainDir="data/medium_sentences.pkl"  # sliding window data for extracting inference
 
 # Model saving arguments
 modelDir="model/imdb_weights"  # better to make an empty directory just for the BERT model
@@ -18,21 +17,22 @@ trainDataDir="data/imdb-train.pkl"  # data to train the complete BERT model
 testDataDir="data/imdb-test.pkl"
 
 # Activation inference arguments
-activationDir="data/activations.npy"  # dir of .npy file to save dataset embeddings
+activationDir="data/medium_activations.npy"  # dir of .npy file to save dataset embeddings
+batchSizeInference=128
 
 # Clustering arguments
 n_clusters=7
-clusterDir="data/clusters.npy"  # path to clustering results
+clusterDir="data/medium_clusters.npy"  # path to clustering results
 
 # ConceptSHAP arguments
 numConcepts=5
 conceptSHAPModelDir="conceptSHAP/models"  # saving directory for conceptSHAP model
 logDir="conceptSHAP/logs"
 lr=1e-3
-numEpochs=20
-lossRegEpoch=5  # number of epochs to run without loss regularization
-saveInterval=5
-
+numEpochs=150
+lossRegEpoch=10  # number of epochs to run without loss regularization
+saveInterval=155
+batchSizeTraining=1024
 
 # Read command line running option
 # six option flags available: 1 for "data preprocess", 2 for "BERT model training",
@@ -52,7 +52,7 @@ fi
 
 if [ $option -eq 3 ] || [ $option -eq 0 ]; then
     # Extract mid-layer activations
-    sh model/bert_inference.sh $batchSize $trainDir $modelDir $activationDir
+    sh model/bert_inference.sh $batchSizeInference $trainDir $modelDir $activationDir
 fi
 
 if [ $option -eq 4 ] || [ $option -eq 0 ]; then
@@ -64,5 +64,5 @@ if [ $option -eq 5 ] || [ $option -eq 0 ]; then
     # Rest of conceptSHAP
     # TODO: SSH to server port (TensorBoard) to plot the training curve
     sh conceptSHAP/train_eval.sh $activationDir $clusterDir $trainDir $modelDir $numConcepts \
-        $conceptSHAPModelDir $logDir $lr $batchSize $numEpochs $lossRegEpoch $saveInterval
+        $conceptSHAPModelDir $logDir $lr $batchSizeTraining $numEpochs $lossRegEpoch $saveInterval
 fi
